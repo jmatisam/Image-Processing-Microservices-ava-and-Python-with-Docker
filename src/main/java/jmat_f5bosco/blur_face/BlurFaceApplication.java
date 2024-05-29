7d54a6b8d5dc8a8397e1b25220a9b0ce41a6efd2
jmat732 <jmatisamuel@gmail.com>
2024-05-29 22:52:48 +0200

    Root proyect

---
+++ src/main/java/jmat_f5bosco/blur_face/BlurFaceApplication.java
@@ -0,0 +1,13 @@
+package jmat_f5bosco.blur_face;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlurFaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jmat_f5bosco/blur_face/BlurFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package jmat_f5bosco.blur_face;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlurFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
